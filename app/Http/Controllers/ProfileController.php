31469867c750e3021b6a42fbc024b5aa3c55f7f0
Mohammed Seleman <92472515+Mo-Seleman@users.noreply.github.com>
2024-10-30 09:00:26 +0000

    Avatar Upload Functionality

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -22,6 +22,7 @@ class ProfileController extends Controller
         return Inertia::render('Profile/View', [
             'mustVerifyEmail' => $user instanceof MustVerifyEmail,
             'status' => session('status'),
+            'success' => session('success'),
             'user' => new UserResource($user),
         ]);
     }
@@ -76,6 +77,8 @@ class ProfileController extends Controller
         $avatar = $data['avatar'] ?? null;
         // @var \Illuminate\Http\UploadedFile $cover
         $cover = $data['cover'] ?? null;
+
+        $success = '';
         
         if($cover){
 
@@ -85,10 +88,22 @@ class ProfileController extends Controller
 
             $path = $cover->store('user-'.$user->id, 'public');
             $user->update(['cover_path' => $path]);
+            $success = 'Your Cover Image Has Succcessfully Been Updated';
+        }
+
+        if($avatar){
+
+            if ($user->avatar_path){
+                Storage::disk('public')->delete($user->avatar_path);
+            }
+
+            $path = $avatar->store('user-'.$user->id, 'public');
+            $user->update(['avatar_path' => $path]);
+            $success = 'Your Avatar Image Has Succcessfully Been Updated';
 
         }
         
-        return back()->with('status', 'cover-image-updated');
+        return back()->with('success', $success);
 
     }
 }
