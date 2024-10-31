ef17290373c320d98b02655dcadbe0c62bb9f095
Mohammed Seleman <92472515+Mo-Seleman@users.noreply.github.com>
2024-10-31 07:14:06 +0000

    Updating Profile Info
    Users Can Now Change Their Personal Information On The Profile Page

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ Route::get('/u/{user:username}', [ProfileController::class, 'index'])->name('pro
 Route::middleware('auth')->group(function () {
     Route::post('/profile/update-images', [ProfileController::class, 'updateImage'])->name('profile.updateImages');
     // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/my-groups', [GroupController::class, 'index'])->name('my-groups');
     Route::get('/my-friends', [FriendController::class, 'index'])->name('my-friends');
