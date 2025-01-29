7bb03e68897da07607d35ecf751f779b0547e572
Mohammed Seleman <92472515+Mo-Seleman@users.noreply.github.com>
2025-01-29 20:11:10 +0000

    Edit Post
    Implemented Functionality For Users To Edit Their Post

--- resources/js/Components/app/PostItem.vue
+++ resources/js/Components/app/PostItem.vue
@@ -1,10 +1,16 @@
 <script setup>
+    import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
+    import { PencilIcon, TrashIcon, EllipsisVerticalIcon } from '@heroicons/vue/20/solid'
+
     import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue';
     import { ref } from 'vue';
+import PostUserHeader from './PostUserHeader.vue';
 
-    defineProps({
+    const props = defineProps({
         post: Object
     });
+
+    const emit = defineEmits(['editClick'])
 
     function isImage(attachment){
         const mime = attachment.mime.split('/');
@@ -23,27 +29,57 @@
     };
 
 
-    // function togglePostMenu(index){
-    //     showPostMenu.value = !showPostMenu.value;
-    // }
+    function openEditModal(){
+        emit('editClick', props.post)
+    }
+
 </script>
 
 <template>
     <div class="px-6 pt-6 mb-4 bg-[#f2f8f3] rounded-md shadow">
-        <div>
-            <a href="javascript:void(0)" class="flex items-center gap-2">
-                <img :src="post.user.avatar_url" class="w-[52px] rounded-full transition-all hover:animate-pulse hover:scale-105">
+        <div class="flex items-center justify-between">
+           <PostUserHeader :post="post" />
+                <Menu as="div" class="relative inline-block text-left">
                 <div>
-                    <div class="flex flex-row gap-2 font-bold text-lg">
-                        <h4 class="hover:underline">{{ post.user.name }}</h4>
-                        <template v-if="post.group">
-                            >
-                            <a href="javascript:void(0)" class="hover:text-[#ff4f40] hover:animate-bounce">{{ post.group.name }}</a>
-                        </template>
-                    </div>
-                    <small>{{ post.created_at }}</small>
+                    <MenuButton class="w-8 h-8 rounded-full hover:bg-black/5 transition flex items-center justify-center">
+                        <EllipsisVerticalIcon class="w-5 h-5" aria-hidden="true"/>
+                    </MenuButton>
                 </div>
-            </a>
+
+                <transition
+                    enter-active-class="transition duration-100 ease-out"
+                    enter-from-class="transform scale-95 opacity-0"
+                    enter-to-class="transform scale-100 opacity-100"
+                    leave-active-class="transition duration-75 ease-in"
+                    leave-from-class="transform scale-100 opacity-100"
+                    leave-to-class="transform scale-95 opacity-0"
+                >
+                    <MenuItems class="absolute right-0 mt-2 w-44 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+                    <div class="px-1 py-1">
+                        <MenuItem v-slot="{ active }">
+                        <button @click="openEditModal" :class="[ active ? 'bg-[#016b83] text-white' : 'text-gray-900', 'group flex w-full items-center rounded-md px-2 py-2 text-sm', ]" >
+                            <PencilIcon class="mr-2 h-4 w-4" aria-hidden="true" />
+                            Edit
+                        </button>
+                        </MenuItem>
+                        <MenuItem v-slot="{ active }">
+                        <button
+                            :class="[
+                            active ? 'bg-[#016b83] text-white' : 'text-gray-900',
+                            'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                            ]"
+                        >
+                            <TrashIcon
+                            class="mr-2 h-4 w-4"
+                            aria-hidden="true"
+                            />
+                            Delete
+                        </button>
+                        </MenuItem>
+                    </div>
+                    </MenuItems>
+                </transition>
+                </Menu>
         </div>
         <div>
             <Disclosure v-slot="{ open }">
