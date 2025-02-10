10bcc052ad3d616e1c444dbf535c0e2f4a09c686
Mohammed Seleman <92472515+Mo-Seleman@users.noreply.github.com>
2025-02-10 08:10:05 +0000

    Edit Post Attachments
    Post Attachments Can Be Added and Removed As Well As Being Saved To Local Storage

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -60,8 +60,54 @@ class PostController extends Controller
      */
     public function update(UpdatePostRequest $request, Post $post)
     {
-        $post->update($request->validated());
-        return back();
+
+        $user = $request->user();
+
+        DB::beginTransaction();
+        $allFilePaths = [];
+        try {
+            $data = $request->validated();
+            $post->update($data);
+
+            $deleted_ids = $data['deleted_files_ids'] ?? [];
+
+            $attachments = PostAttachment::query()
+                ->where('post_id', $post->id)
+                ->whereIn('id', $deleted_ids)
+                ->get();
+
+
+            foreach ($attachments as $attachment) {
+                $attachment->delete();
+            }
+
+
+            /** @var \Illuminate\Http\UploadedFile[] $files */
+            $files = $data['attachments'] ?? [];
+
+            foreach ($files as $file) {
+                $path = $file->store('attachments/' . $post->id, 'public');
+                $allFilePaths[] = $path;
+                PostAttachment::create([
+                    'post_id' => $post->id,
+                    'name' => $file->getClientOriginalName(),
+                    'path' => $path,
+                    'mime' => $file->getMimeType(),
+                    'size' => $file->getSize(),
+                    'created_by' => $user->id,
+                ]);
+            }
+
+            DB::commit();
+            } catch (\Throwable $th) {
+                foreach ($allFilePaths as $path) {
+                    Storage::disk('public')->delete($path);
+                }
+                DB::rollBack();
+                throw $th;
+            }
+
+            return back();
     }
 
     /**
--- resources/js/Components/app/PostModal.vue
+++ resources/js/Components/app/PostModal.vue
@@ -1,7 +1,7 @@
 <script setup>
-import { ref, computed, watch } from 'vue'
+import { ref, computed, watch, watchEffect } from 'vue'
 import { TransitionRoot, TransitionChild, Dialog, DialogPanel, DialogTitle, } from '@headlessui/vue'
-import { XMarkIcon, PaperClipIcon, PaperAirplaneIcon } from '@heroicons/vue/24/solid'
+import { XMarkIcon, PaperClipIcon, PaperAirplaneIcon, ArrowUturnLeftIcon } from '@heroicons/vue/24/solid'
 import PostUserHeader from './PostUserHeader.vue';
 import { useForm } from '@inertiajs/vue3';
 import Editor from './Editor.vue';
@@ -20,9 +20,10 @@ const emit = defineEmits(['update:modelValue'])
 const attachmentFiles = ref([])
 
 const form = useForm({
-    id: null,
     body: '',
-    attachments: []
+    attachments: [],
+    deleted_files_ids: [],
+    _method: 'POST',
 })
 
 const show = computed({
@@ -30,21 +31,28 @@ const show = computed({
     set: (value) => emit('update:modelValue', value)
 })
 
+const computedAttachments = computed(() => {
+    return [...attachmentFiles.value, ...(props.post.attachments || [])]     
+})
+ 
 function closeModal() {
   show.value = false
   emit('update:modelValue', false)
+  emit('hide')
   resetModal()
 }
 
 function resetModal() {
     form.reset()
     attachmentFiles.value = []
+    props.post.attachments.forEach(file => file.deleted = false);
 }
 
-function submit() { 
+function submit() {     
     form.attachments = attachmentFiles.value.map(myFile => myFile.file)
-    if(form.id){
-            form.put(route('post.update', props.post.id), {
+    if(props.post.id){    
+            form._method = 'PUT'
+            form.post(route('post.update', props.post.id), {
             preserveScroll: true,
             onSuccess: () => {
                 show.value = false;
@@ -63,12 +71,12 @@ function submit() {
 }
 
 watch(() => props.post, () => {
-        form.id = props.post.id
-        form.body = props.post.body
-    })
+    console.log("%c Watch is Triggered", "color: orange; font-size: 16px;", props.post);
+    form.body = props.post.body || ''
+})
 
 async function onAttachmentChoose($event) {
-    $event.target.files = null;
+    // $event.target.files = null;
     console.log($event.target.files)
     for (const file of $event.target.files) {
         const myFile = {
@@ -77,7 +85,7 @@ async function onAttachmentChoose($event) {
         }
         attachmentFiles.value.push(myFile)
     }
-    console.log(attachmentFiles.value)
+    console.log("onAttachmentChoose", attachmentFiles.value)
 }
 
 async function readFile(file) {
@@ -96,8 +104,18 @@ async function readFile(file) {
 } 
 
 function removeFile(myFile){
-    attachmentFiles.value = attachmentFiles.value.filter(f => f !== myFile)
-};
+    if(myFile.file){
+        attachmentFiles.value = attachmentFiles.value.filter(f => f !== myFile);
+    } else {
+        form.deleted_files_ids.push(myFile.id);
+        myFile.deleted = true;
+    }
+}
+
+function undoDelete(myFile){
+    myFile.deleted = false;
+    form.deleted_files_ids = form.deleted_files_ids.filter(id => myFile.id != id)
+}
 
 </script>
 
@@ -113,16 +131,16 @@ function removeFile(myFile){
                         <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0 scale-95" enter-to="opacity-100 scale-100" leave="duration-200 ease-in" leave-from="opacity-100 scale-100" leave-to="opacity-0 scale-95">
                             <DialogPanel class="w-full max-w-md transform overflow-hidden rounded bg-white text-left align-middle shadow-xl transition-all" >
                                 <DialogTitle as="h3" class="flex items-center justify-between py-3 px-4 font-medium bg-gray-100 text-gray-900">
-                                    <h4>{{ form.id ? 'Update Post' : 'Create Post' }}</h4>
-                                    <button @click="show = false" class="w-8 h-8 rounded-full hover:bg-black/5 transition flex items-center justify-center">
+                                    <h4>{{ post.id ? 'Update Post' : 'Create Post' }}</h4>
+                                    <button @click="closeModal" class="w-8 h-8 rounded-full hover:bg-black/5 transition flex items-center justify-center">
                                         <XMarkIcon class="w-4 h-4" />
                                     </button>
                                 </DialogTitle>
                                 <div class="p-4">
                                     <PostUserHeader :post="post" :showTime="false" class="mb-4"/>
                                     <Editor v-model="form.body"/>
-                                    <div class="grid gap-3 my-3" :class="[ attachmentFiles.length === 1 ? 'grid-cols-1' : 'grid-cols-2' ]">
-                                        <div v-for="(myFile, index) of attachmentFiles" :key="index" class="relative">
+                                    <div class="grid gap-3 my-3" :class="[ computedAttachments.length === 1 ? 'grid-cols-1' : 'grid-cols-2' ]">
+                                        <div v-for="(myFile, index) of computedAttachments" :key="index" class="relative">
                                             <!-- <button :class="['w-6 h-6 absolute right-3 top-3 cursor-pointer rounded-sm', { 'text-white bg-black': showPostMenu, 'text-black bg-white': !showPostMenu }]">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
@@ -137,17 +155,22 @@ function removeFile(myFile){
                                                 </div>
                                             </Transition> -->
 
+                                            <div v-if="myFile.deleted" class="absolute left-0 bottom-0 right-0 py-2 px-3 bg-black text-white flex justify-between items-center z-10">
+                                                <p>To Be Deleted</p>
+                                                <ArrowUturnLeftIcon @click="undoDelete(myFile)" class="size-4 cursor-pointer"/>
+                                            </div>
+
                                             <button @click="removeFile(myFile)"
                                                 class="absolute z-20 right-3 top-3 w-7 h-7 flex items-center justify-center bg-black/30 hover:bg-black/40 text-white rounded-full">
                                                 <XMarkIcon class="size-4" />
                                             </button>
 
-                                            <img v-if="isImage(myFile.file)" :src="myFile.url" class="bg-cover bg-no-repeat object-contain aspect-square rounded-lg">
-                                            <div v-else class="aspect-square bg-blue-100 flex flex-col items-center justify-center text-gray-500 hover:text-black active:text-black cursor-pointer rounded-lg">
+                                            <img v-if="isImage(myFile.file || myFile)" :src="myFile.url" class="bg-cover bg-no-repeat object-contain aspect-square rounded-lg" :class="[myFile.deleted ? 'opacity-50' : '']">
+                                            <div v-else class="aspect-square bg-blue-100 flex flex-col items-center justify-center text-gray-500 hover:text-black active:text-black cursor-pointer rounded-lg" :class="[myFile.deleted ? 'opacity-50' : '']">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-16 h-16">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
                                                 </svg>
-                                                <small class="text-center px-1"> {{ myFile.file.name }} </small>
+                                                <small class="text-center px-1"> {{ (myFile.file || myFile).name }} </small>
                                             </div>
                                         </div>
                                     </div>
