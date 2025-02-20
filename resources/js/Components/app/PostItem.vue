70d7bea4f9edb902ac36584d0328f4082d9c93c0
Mohammed Seleman <92472515+Mo-Seleman@users.noreply.github.com>
2025-02-20 16:44:14 +0000

    Updating & Deleting Comments
    Feature: Update and delete existing comments

--- resources/js/Components/app/PostItem.vue
+++ resources/js/Components/app/PostItem.vue
@@ -1,6 +1,4 @@
 <script setup>
-    import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
-    import { PencilIcon, TrashIcon, EllipsisVerticalIcon } from '@heroicons/vue/20/solid'
     import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue';
     import { ref } from 'vue';
     import PostUserHeader from './PostUserHeader.vue';
@@ -10,9 +8,10 @@
     import axiosClient from "@/axiosClient.js"
     import TextareaInput from '../TextareaInput.vue';
     import IndigoButton from './indigoButton.vue';
+import EditDeleteDropdown from './EditDeleteDropdown.vue';
 
     const authUser = usePage().props.auth.user
- 
+    const editingComment = ref({})
     const props = defineProps({
         post: Object
     });
@@ -72,45 +71,49 @@
         })
     }
 
+    function deleteComment(comment) {
+        if (window.confirm('Are you sure you want to delete this comment?')) {
+                axiosClient.delete(route('post.comment.delete', comment.id))
+                .then(({ data }) => {
+                    props.post.comments = props.post.comments.filter(c => c.id !== comment.id)
+                    props.post.num_of_comments--;
+                })
+        } {
+            return false;
+        }
+    }
+
+    function startCommentEdit(comment){
+        console.log(comment);
+        editingComment.value = {
+            id: comment.id,
+            comment: comment.comment.replace(/(<br\s*\/?>\s*)+/gi, '\n')
+        }
+    }
+
+    function updateComment(){
+        axiosClient.put(route('post.comment.update', editingComment.value.id), {
+            comment: editingComment.value.comment
+        })
+
+        .then(({ data }) => {
+            editingComment.value = null
+            props.post.comments = props.post.comments.map((c) => {
+                if (c.id === data.id){
+                    return data
+                }
+                return c;
+            })
+        })
+    }
+
 </script>
 
 <template>
     <div class="p-6 mb-4 bg-[#f2f8f3] rounded-md shadow">
         <div class="flex items-center justify-between">
            <PostUserHeader :post="post" />
-                <Menu as="div" class="relative z-10 inline-block text-left">
-                <div>
-                    <MenuButton class="w-8 h-8 rounded-full hover:bg-black/5 transition flex items-center justify-center">
-                        <EllipsisVerticalIcon class="w-5 h-5" aria-hidden="true"/>
-                    </MenuButton>
-                </div>
-
-                <transition
-                    enter-active-class="transition duration-100 ease-out"
-                    enter-from-class="transform scale-95 opacity-0"
-                    enter-to-class="transform scale-100 opacity-100"
-                    leave-active-class="transition duration-75 ease-in"
-                    leave-from-class="transform scale-100 opacity-100"
-                    leave-to-class="transform scale-95 opacity-0"
-                >
-                    <MenuItems class="absolute right-0 mt-2 w-44 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
-                    <div class="px-1 py-1">
-                        <MenuItem v-slot="{ active }">
-                        <button @click="openEditModal" :class="[ active ? 'bg-[#016b83] text-white' : 'text-gray-900', 'group flex w-full items-center rounded-md px-2 py-2 text-sm', ]" >
-                            <PencilIcon class="mr-2 h-4 w-4" aria-hidden="true" />
-                            Edit
-                        </button>
-                        </MenuItem>
-                        <MenuItem v-slot="{ active }">
-                        <button @click="deletePost" :class="[ active ? 'bg-[#016b83] text-white' : 'text-gray-900', 'group flex w-full items-center rounded-md px-2 py-2 text-sm', ]">
-                            <TrashIcon class="mr-2 h-4 w-4" aria-hidden="true" />
-                            Delete
-                        </button>
-                        </MenuItem>
-                    </div>
-                    </MenuItems>
-                </transition>
-                </Menu>
+           <EditDeleteDropdown :user="post.user"  @edit="openEditModal" @delete="deletePost"/>
         </div>
         <div>
             <Disclosure v-slot="{ open }">
@@ -160,21 +163,31 @@
                     </a>
                     <div class="flex flex-1">
                         <TextareaInput v-model="newCommentText" rows="1" maxlength="400" class="w-full overflow-auto resize-none rounded-r-none max-h-[85px]" placeholder="Enter your comment here"/>
-                        <IndigoButton @click="createComment" class="w-[80px] rounded-l-none">Submit</IndigoButton>
+                        <IndigoButton @click="createComment" class="w-[100px] rounded-l-none">Submit</IndigoButton>
                     </div>
                 </div>
                 <div class="overflow-scroll max-h-[440px]">
                     <div v-for="comment of post.comments" :key="comment.id" class="mb-4">
-                        <div class="flex gap-2">
-                            <a href="javascript:void(0)">
-                                <img :src="comment.user.avatar_url" class="w-[52px] rounded-full">
-                            </a>
-                            <div>
-                                <h4 class="hover:underline">{{ comment.user.name }}</h4>
-                                <small class="text-xs text-gray-500">{{ comment.updated_at }}</small>
+                        <div class="flex justify-between gap-2">
+                            <div class="flex gap-2">
+                                <a href="javascript:void(0)">
+                                    <img :src="comment.user.avatar_url" class="w-[52px] rounded-full">
+                                </a>
+                                <div>
+                                    <h4 class="hover:underline">{{ comment.user.name }}</h4>
+                                    <small class="text-xs text-gray-500">{{ comment.updated_at }}</small>
+                                </div>
+                            </div>
+                            <EditDeleteDropdown :user="comment.user" @edit="startCommentEdit(comment)" @delete="deleteComment(comment)"/>
+                        </div>
+                        <div v-if="editingComment && editingComment.id === comment.id" class="ml-12 max-w-[90%]">
+                            <TextareaInput v-model="editingComment.comment" rows="1" maxlength="400" class="w-full overflow-auto resize-none rounded-r-none max-h-[85px] px-[0.94rem]" placeholder="Enter your comment here"/>
+                            <div class="flex gap-2 justify-end ">
+                                <button @click="editingComment = {}" class="text-gray-800">cancel</button>
+                                <IndigoButton @click="updateComment" class="w-[100px] rounded-l-none">update</IndigoButton>
                             </div>
                         </div>
-                        <div class="text-sm flex flex-1 ml-16 [line-break:anywhere]" v-html="comment.comment">
+                        <div v-else class="text-md flex flex-1 items-center ml-16 [line-break:auto] min-h-[41px] max-w-[85%] py-2" v-html="comment.comment">
                         </div>
                     </div>
                 </div>
