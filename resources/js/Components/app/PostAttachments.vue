d7777026db5f4a60c8ced602139bfdce37f915fe
Mohammed Seleman <92472515+Mo-Seleman@users.noreply.github.com>
2025-02-27 08:06:18 +0000

    Comments Table Update and New Attachments Component
    Updated Comments Model To Accept Parent ID - Preparing For Nested Comments Functionality. Also Extracted New Attachments Component From PostItem

---
+++ resources/js/Components/app/PostAttachments.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ArrowDownTrayIcon, PaperClipIcon } from '@heroicons/vue/24/solid';
+import { isImage } from '@/helpers';
+
+defineProps({
+    attachments: Array
+});
+
+defineEmits(['attachmentClick']);
+
+
+</script>
+
+<template>
+    <div @click="$emit('attachmentClick', index)" v-for="(attachment, index) of attachments.slice(0, 4)" :key="index" class="relative cursor-pointer">
+        <div v-if="index === 3 && attachments.length > 4" class="absolute left-0 top-0 right-0 bottom-0 z-10 bg-black/60 text-white flex items-center justify-center text-2xl">
+            <p>+{{ attachments.length - 4}}</p>
+        </div>
+        <a @click.stop :href="route('post.download', attachment)" class="z-20 group-hover:opacity-100 transition-all w-7 h-7 flex items-center justify-center text-gray-100 bg-gray-700 rounded absolute right-2 top-2 cursor-pointer hover:bg-gray-800">
+            <ArrowDownTrayIcon class="size-4"/>
+        </a>
+        <img v-if="isImage(attachment)" :src="attachment.url" class="bg-cover bg-no-repeat object-contain aspect-square rounded-lg">
+        <div v-else class="aspect-square bg-blue-100 flex flex-col items-center justify-center text-gray-500 hover:text-black active:text-black cursor-pointer rounded-lg">
+            <PaperClipIcon size="4"/>
+            <small>{{ attachment.name }}</small>
+        </div>
+    </div>
+</template>
--- resources/js/Components/app/PostItem.vue
+++ resources/js/Components/app/PostItem.vue
@@ -10,6 +10,7 @@
     import TextareaInput from '../TextareaInput.vue';
     import IndigoButton from './indigoButton.vue';
     import EditDeleteDropdown from './EditDeleteDropdown.vue';
+    import PostAttachments from './PostAttachments.vue';
 
     const authUser = usePage().props.auth.user
     const editingComment = ref({})
@@ -143,19 +144,7 @@
             </Disclosure>
         </div>
         <div class="grid gap-3" :class="[ post.attachments.length === 1 ? 'grid-cols-1' : 'grid-cols-2' ]">
-            <div @click="openAttachment(index)" v-for="(attachment, index) of post.attachments.slice(0, 4)" :key="index" class="relative cursor-pointer">
-                <div v-if="index === 3 && post.attachments.length > 4" class="absolute left-0 top-0 right-0 bottom-0 z-10 bg-black/60 text-white flex items-center justify-center text-2xl">
-                    <p>+{{ post.attachments.length - 4}}</p>
-                </div>
-                <a @click.stop :href="route('post.download', attachment)" class="z-20 group-hover:opacity-100 transition-all w-7 h-7 flex items-center justify-center text-gray-100 bg-gray-700 rounded absolute right-2 top-2 cursor-pointer hover:bg-gray-800">
-                    <ArrowDownTrayIcon class="size-4"/>
-                </a>
-                <img v-if="isImage(attachment)" :src="attachment.url" class="bg-cover bg-no-repeat object-contain aspect-square rounded-lg">
-                <div v-else class="aspect-square bg-blue-100 flex flex-col items-center justify-center text-gray-500 hover:text-black active:text-black cursor-pointer rounded-lg">
-                    <PaperClipIcon size="4"/>
-                    <small>{{ attachment.name }}</small>
-                </div>
-            </div>
+            <PostAttachments :attachments="post.attachments" @attachmentClick="openAttachment"/>
         </div>
         <Disclosure v-slot="{ open }">
         <div class="flex gap-3 py-4">
@@ -219,6 +208,10 @@
                                     <ChatBubbleLeftEllipsisIcon class="size-4"/>
                                     Reply
                                 </button>
+                            </div>
+                            <div class="flex flex-1 mt-2">
+                                <TextareaInput v-model="newCommentText" rows="1" maxlength="400" class="w-full overflow-auto resize-none rounded-r-none max-h-[85px]" placeholder="Enter your comment here"/>
+                                <IndigoButton @click="createComment" class="w-[100px] rounded-l-none">Submit</IndigoButton>
                             </div>
                         </div>
                     </div>
