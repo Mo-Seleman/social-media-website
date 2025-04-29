0ea6703a70f29676d4a2159f6f6cb94c7114b87d
Mohammed Seleman <92472515+Mo-Seleman@users.noreply.github.com>
2025-04-29 08:06:48 +0100

    Update Group Info

--- app/Http/Requests/UpdateGroupRequest.php
+++ app/Http/Requests/UpdateGroupRequest.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Group;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateGroupRequest extends FormRequest
@@ -11,7 +13,12 @@ class UpdateGroupRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        /**
+         * @var Group
+         */
+        $group = $this->route('group');
+
+        return $group->isAdmin(Auth::id());
     }
 
     /**
@@ -22,7 +29,9 @@ class UpdateGroupRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', 'max:255'],
+            'auto_approval' => ['required', 'boolean'],
+            'about' => ['nullable']
         ];
     }
 }
---
+++ resources/js/Components/app/GroupForm.vue
@@ -0,0 +1,28 @@
+        
+<script setup>
+import TextareaInput from '../TextareaInput.vue';
+import Checkbox from '@/Components/Checkbox.vue';
+import TextInput from '@/Components/TextInput.vue';
+
+defineProps({
+    form: Object
+});
+
+</script>
+
+<template>
+    <div class="mb-3">
+        <label class="capitalize">Group name</label>
+        <TextInput type="text" class="mt-1 block w-full" v-model="form.name" required autofocus />
+    </div>
+    <div class="mb-3">
+        <label class="capitalize">
+            <Checkbox name="remember" v-model:checked="form.auto_approval" />
+            Enable auto approval
+        </label>
+    </div>
+    <div class="mb-3">
+        <label class="capitalize">About group</label>
+        <TextareaInput v-model="form.about" class="w-full" />
+    </div>
+</template>
