8ed7145a6ecc3d36ef393ee75fa2464ec1ac2a8a
Mohammed Seleman <92472515+Mo-Seleman@users.noreply.github.com>
2025-06-09 10:16:28 +0100

    myFriends Page
    Passed Users Props Down

--- resources/js/Pages/MyFriends.vue
+++ resources/js/Pages/MyFriends.vue
@@ -1,12 +1,17 @@
 <script setup>
 import FollowingList from '@/Components/app/FollowingList.vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+
+const props = defineProps({
+  following: Array,
+});
+
 </script>
 
 <template>
 <div>
     <AuthenticatedLayout>
-        <FollowingList />
+        <FollowingList :users="following"/>
     </AuthenticatedLayout>
   </div>
 </template>
